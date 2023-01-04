22ef0498483a3340a6f43c2ce18e6f3e72f75582
rukkiecodes <rukkiecodes@gmail.com>
2023-01-04 22:38:19 +0100

    update traders

--- src/views/app/routes/WithdrawRequestCmponents/User.vue
+++ src/views/app/routes/WithdrawRequestCmponents/User.vue
@@ -1,11 +1,21 @@
 <template>
   <td>
-    <span class="text-body-2 font-weight-bold text-grey-darken-3 hidden-sm-and-up">{{ currentUser?.name }}</span>
+    <span
+      class="text-body-2 font-weight-bold text-grey-darken-3 hidden-sm-and-up"
+      >{{ currentUser?.name }}</span
+    >
     <v-list class="hidden-xs">
       <v-list-item>
         <template v-slot:prepend>
           <v-avatar size="45" class="mr-3 rounded-lg" rounded="0">
-            <v-img :src="currentUser?.avatar" cover />
+            <v-img
+              :src="
+                currentUser?.avatar != undefined
+                  ? currentUser?.avatar
+                  : 'https://cdn.vuetifyjs.com/images/lists/1.jpg'
+              "
+              cover
+            />
           </v-avatar>
         </template>
 
